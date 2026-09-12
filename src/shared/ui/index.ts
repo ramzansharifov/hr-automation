766e2cb8372c0d1ec7362f28cdc7bc8e797001e6
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-09-13 00:57:59 +0500

    refactor: export shared button primitives

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
-export { Button } from './Button'
+export { Button, type ButtonProps } from './Button'
+export { ChoiceButton, type ChoiceButtonProps } from './ChoiceButton'
 export { ConfirmDialog } from './ConfirmDialog'
 export { DataTable, type DataTableColumn } from './DataTable'
 export { DatePicker, type DatePickerProps } from './DatePicker'
